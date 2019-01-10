79dd466d2ceca3542ac42b82184c7b8e0092e98c
Lcarused <donghao19950925@gmail.com>
2019-01-10 13:01:27 +0800

    admin post界面功能优化

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category
 
+# https://blog.csdn.net/youand_me/article/details/78831494
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+    list_display = ['title', 'show_time', 'category', 'author', 'state_color']
+    list_filter = ['show_time', 'category', 'state']
+    search_fields = ('title',)
+    list_per_page = 10000
 
 
+admin.site.site_header = 'Lcarusd Blog Admin'
+admin.site.site_title = 'Lcarusd Blog Admin'
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
-admin.site.register(Tag)
