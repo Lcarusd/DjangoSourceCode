daff9409f203095de6a2632cc3424306b57b42e1
Lcarused <donghao19950925@gmail.com>
2019-01-10 15:36:19 +0800

    删除评论与搜索app，并删除相关依赖文件

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -35,9 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'haystack',
     'blog',
-    'comments',
     'markdownx',
 ]
 
