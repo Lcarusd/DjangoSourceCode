ddb01440dc5ea015fc7b3786ef765fca63635127
Lcarused <donghao19950925@gmail.com>
2019-01-16 19:27:05 +0800

    request请求信息收集后台管理

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Seek
+from .models import Post, Category, Seek, RequestInfo
 
 # https://blog.csdn.net/youand_me/article/details/78831494
 
@@ -15,9 +15,15 @@ class SeekAdmin(admin.ModelAdmin):
     ordering = ['-show_time', ]
     list_per_page = 10000
 
+class RequestInfoAdmin(admin.ModelAdmin):
+    list_display = ['ip_address', 'area', 'request_time', 'request_path', 'device', 'system', 'browser']
+    ordering = ['-request_time', ]
+
+
 
 admin.site.site_header = 'Lcarusd Blog Admin'
 admin.site.site_title = 'Lcarusd Blog Admin'
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Seek, SeekAdmin)
+admin.site.register(RequestInfo, RequestInfoAdmin)
