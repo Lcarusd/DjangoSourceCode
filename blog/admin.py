a87199a38ee705113107451a5bc945c29b4ac164
Lcarused <donghao19950925@gmail.com>
2019-01-18 19:41:32 +0800

    新增运动数据管理后台配置项

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Seek, RequestInfo
+from .models import Post, Category, Seek, RequestInfo, Sports
 
 # https://blog.csdn.net/youand_me/article/details/78831494
 
@@ -13,11 +13,16 @@ class SeekAdmin(admin.ModelAdmin):
     list_display = ['name', 'content', 'show_time']
     search_fields = ('content',)
     ordering = ['-show_time', ]
-    list_per_page = 10000
+    list_per_page = 1000
 
 class RequestInfoAdmin(admin.ModelAdmin):
     list_display = ['ip_address', 'area', 'request_time', 'request_path', 'device', 'system', 'browser']
     ordering = ['-request_time', ]
+
+
+class SportsAdmin(admin.ModelAdmin):
+    list_display = ['start_time', 'end_time', 'step', 'km', 'kcal']
+    ordering = ['-start_time', ]
 
 
 
@@ -27,3 +32,4 @@ admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Seek, SeekAdmin)
 admin.site.register(RequestInfo, RequestInfoAdmin)
+admin.site.register(Sports, SportsAdmin)
