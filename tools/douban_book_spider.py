a59e1b3af2df2d25cffc199ddb6fb418ffdb206a
Lcarused <donghao19950925@gmail.com>
2019-01-31 17:30:04 +0800

    豆瓣读书记录爬虫骨架

--- tools/douban_book_spider.py
+++ tools/douban_book_spider.py
@@ -1,3 +1,28 @@
 # -*- coding:utf-8 -*-
 
 """豆瓣读书记录爬虫"""
+
+class DoubanBookSpider(object):
+    def __init__(self):
+        pass
+
+    def get_page_sum(self):
+        pass
+
+    def page_data_spider(self):
+        pass
+
+    def parse_book_data(self):
+        pass
+
+    @staticmethod
+    def main():
+        pass
+
+
+if __name__ == "__main__":
+    DoubanBookSpider.main()
+
+'''
+url = "https://book.douban.com/people/145966625/collect?start=15&sort=time&rating=all&filter=all&mode=grid"
+'''
